c8b2c96074c282646902162452e0413b3b574555
WMXPY <wm@mengw.io>
2017-10-20 23:25:15 -0500

    init

---
+++ src/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.post('/api/*', function (req, res) {
+    console.log(req.body);
+    res.header("Access-Control-Allow-Origin", "*");
+    res.send('Hello Worlds');
+})
+
+app.get('*', function (req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.send('Hello World');
+})
+
+var server = app.listen(8081, function () {
+
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("应用实例，访问地址为 http://%s:%s", host, port)
+
+})
