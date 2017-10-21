f1011efd5c5d9b9e8ed4efbb534bd8fd41bcbe47
PCXPY <pc@wmpcxpy.com>
2017-10-21 16:48:27 -0500

    [Clean] Remove albacore from server

--- src/test.js
+++ src/test.js
@@ -1,3 +1,9 @@
 const dace = require('../albacoreDist/albacore').default;
 
 let a = new dace('albacoreDB');
+a.writeTable("test", {
+    a: 20,
+    b: 1
+})
+let b = a.table("test")
+console.log(b)
